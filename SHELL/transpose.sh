36ac32882e4bc4201f30f410167046932a4edd19
Lavender1234 <924417727@qq.com>
2018-09-06 13:46:39 +0000

    20180906

---
+++ SHELL/transpose.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+exec awk '
+NR==1{
+	n=NF
+	for (i=1;i<=NF; i++)
+		row[i]=$i
+	next
+}
+{
+	if (NF > n)
+		n=NF
+	for(i = 1;i <= NF; i++)
+		row[i] = row[i] " " $i
+}
+END{
+	for (i=1; i<= n;i++)
+		print row[i]
+}' ${1+"$@"}
